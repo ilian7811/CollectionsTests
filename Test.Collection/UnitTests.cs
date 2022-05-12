71bc521906fdcafb634a915e1bf908eb892512ed
ilian <ilian78@gmail.com>
2022-05-12 11:30:43 +0300

    Add new tests

--- Test.Collection/UnitTests.cs
+++ Test.Collection/UnitTests.cs
@@ -148,9 +148,9 @@ namespace Collection.Test
             nums.AddRange(Enumerable.Range(1, itemsCount).ToArray());
 
             //Asert
-            Assert.That( nums.Count, Is.EqualTo(itemsCount));
-            Assert.That( nums[itemsCount - 2].ToString, Is.EqualTo("999999") );
-            Assert.That( nums[0].ToString, Is.EqualTo("1"));
+            Assert.That(nums.Count, Is.EqualTo(itemsCount));
+            Assert.That(nums[itemsCount - 2].ToString, Is.EqualTo("999999"));
+            Assert.That(nums[0].ToString, Is.EqualTo("1"));
         }
 
         //10
@@ -186,6 +186,80 @@ namespace Collection.Test
 
             //Assert
             Assert.That(nums[firstIndex], Is.EqualTo(7));
+        }
+
+        //12
+        [Test]
+        public void TestCollection_SetByIndex()
+        {
+            //Arrange
+            var nums = new Collection<int>(2, 3, 4, 5);
+            var addIndex = 1;
+            var addNumber = 9;
+            //Act
+            nums.InsertAt(addIndex, addNumber);
+
+            //Assert
+            Assert.That(nums[addIndex], Is.EqualTo(addNumber));
+        }
+
+        //13
+        [Test]
+        public void TestCollection_SetByInvalidIndex()
+        {
+            //Arrange
+            var nums = new Collection<int>(2, 3, 4, 5);
+            var invalidNegativeIndex = -1;
+            var invalidPositivetiveIndex = 4;
+
+            //Act
+
+            //Assert            
+            Assert.That(() => { var element = nums[invalidNegativeIndex]; }, Throws.InstanceOf<ArgumentOutOfRangeException>(), "Get from invalid Negative Index exception");
+            Assert.That(() => { var element = nums[invalidPositivetiveIndex]; }, Throws.InstanceOf<ArgumentOutOfRangeException>(), "Get from invalid Positivetive Index exception");
+        }
+
+        //14
+        [Test]
+        public void TestCollection_RemoveAtStart()
+        {
+            //Arrange
+            var nums = new Collection<int>(2, 3, 4, 5);
+            var firstItemAtPosition = 0;
+            //Act
+            nums.RemoveAt(firstItemAtPosition);
+
+            //Assert            
+            Assert.That(nums[firstItemAtPosition], Is.EqualTo(3));
+        }
+
+        //15
+        [Test]
+        public void TestCollection_RemoveAtEnd()
+        {
+            //Arrange
+            var nums = new Collection<int>(2, 3, 4, 5);
+            var lastrItemAtPosition = 3;
+            //Act
+            nums.RemoveAt(lastrItemAtPosition);
+
+            //Assert            
+            Assert.That(nums[lastrItemAtPosition - 1], Is.EqualTo(4));
+        }
+
+        //15
+        [Test]
+        public void TestCollection_RemoveAtMiddle()
+        {
+            //Arrange
+            var nums = new Collection<int>(2, 3, 4, 5, 6);
+            var midleItemAtPosition = 2;
+
+            //Act
+            nums.RemoveAt(midleItemAtPosition);
+
+            //Assert            
+            Assert.That(nums[midleItemAtPosition], Is.EqualTo(5), "Remove Middle position [2]. Expect position [3] shift left with one.");
         }
 
     }
